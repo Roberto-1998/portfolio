905501f5c77c71559f4096e0485b35560d5ee920
Roberto <rcastellong98@gmail.com>
2023-03-18 21:52:40 +0100

    [add] Navbar and 3D header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { menu, close } from '../assets';
+import NavLinks from './shared/NavLinks';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  const handleActive = (value) => {
+    setActive(value);
+  };
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to={'/'}
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
+        >
+          ROBECODE
+        </Link>
+        <NavLinks active={active} handleActive={handleActive} handleToggle={handleToggle} />
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[20px] h-[20px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? 'hidden' : 'flex'
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}
+          >
+            <NavLinks active={active} handleActive={handleActive} isSmart handleToggle={handleToggle} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/shared/NavLinks.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { navLinks } from '../../constants';
+
+const NavLinks = ({ active, handleActive, isSmart = false, handleToggle }) => {
+  return (
+    <ul
+      className={
+        isSmart ? 'list-none flex justify-end items-start flex-col gap-4 ' : 'list-none hidden sm:flex flex-row gap-10'
+      }
+    >
+      {navLinks.map((link) => (
+        <li
+          onClick={() => {
+            handleActive(link.title);
+            handleToggle();
+          }}
+          key={link.id}
+          className={`
+           ${active === link.title ? 'text-red-500' : 'text-secondary'}
+            ${
+              isSmart
+                ? 'font-poppins font-medium cursor-pointer text-[16px] '
+                : 'hover:text-white text-[16px] font-medium cursor-pointer'
+            }
+        
+        `}
+        >
+          <a href={`#${link.id}`}>{link.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavLinks;
